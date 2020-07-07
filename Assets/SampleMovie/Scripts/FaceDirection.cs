d00635df64bac09f8d1f859e5fd6b1741f033952
ShintaroSaeki <63334692+ShintaroSaeki@users.noreply.github.com>
2020-07-07 14:42:04 +0900

    顔の向きをカメラの方向に向けるようにした

---
+++ Assets/SampleMovie/Scripts/FaceDirection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceDirection : MonoBehaviour
+{
+    [SerializeField] protected Transform objectTransform;
+
+    private Vector3 lookAtPosition;
+    private Quaternion rot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = Quaternion.Euler(-90, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float positionX = objectTransform.position.x;
+        float positionY = this.transform.position.y;
+        float positionZ = objectTransform.position.z;
+
+        lookAtPosition = new Vector3(positionX, positionY, positionZ); 
+
+        this.GetComponent<Transform>().LookAt(lookAtPosition);
+        this.transform.rotation *= rot;
+    }
+}
